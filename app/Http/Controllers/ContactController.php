c181492603633ceb08507703bb91c2f0aa597339
Mark O'Connor <mark.oconnor14@gmail.com>
2022-07-08 16:23:57 +0700

    feat: connect contact form to db

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use Carbon\Carbon;
+
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function store() {
-        dd('You have successfully tried to store something!');
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'message' => 'required',
+        ]);
+
+        $today = Carbon::today('Asia/Taipei')->toDateString();
+        
+        $contact = new Contact;
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->subject = $today .': Email from Portfolio 2022';
+        $contact->message = $request->message;
+        $contact->save();
+
+        // dd($contact);
+        return back()->with('success', 'Hi, ' 
+        . $request->name 
+        . '! Thank you for contacting me! 
+        I will get back to you as soon as possible.'
+    );
     }
 }
