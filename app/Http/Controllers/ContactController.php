72e48c1c124ab1459d8a0ee8ba6c08efa427f4f9
Mark O'Connor <mark.oconnor14@gmail.com>
2022-07-08 15:26:54 +0700

    feat: init contacts controller and web route

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function store() {
+        dd('You have successfully tried to store something!');
+    }
+}
