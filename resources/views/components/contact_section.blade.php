6eeeb0290bbb14b21a438416cf0476e1b2ecb94b
Mark O'Connor <mark.oconnor14@gmail.com>
2022-07-17 15:03:58 +0700

    testcontact grid layout fix overflow

--- resources/views/components/contact_section.blade.php
+++ resources/views/components/contact_section.blade.php
@@ -2,49 +2,49 @@
     <h2 class="heading heading-margin contact-heading">
         <span>Contact</span>
     </h2>  
-    <div>
-            <p class="contact-phrase">
-            {{-- <p class="contact-grid-two contact-phrase"> --}}
-                How I can help you reach your goals?
-            </p>
+    <div class="grid contact-grid">
+        <p class="contact-phrase contact-two">
+        {{-- <p class="contact-grid-two contact-phrase"> --}}
+            How I can help you reach your goals?
+        </p>
         {{-- <div class="contact-one"> --}}
-            <form 
-                action="contact-me" 
-                method="POST"
-                class="contact-form"
-                {{-- class="contact-grid-one contact-form" --}}
-            >    
-                @csrf
-                <div class="flex control-group">
-                    <label class="text-highlight" for="name">Name</label>
-                    <input id="name" name="name" type="text" required>
-                    @error('name')
-                    <p class="error">{{$message}}</p>
-                    @enderror
-                </div>
-                <div class="flex control-group">
-                    <label class="text-highlight" for="email">Email</label>
-                    <input id="email" name="email" type="text" required>
-                    @error('email')
-                    <p class="error">{{$message}}</p>
-                    @enderror
-                </div>
-                <div class="flex control-group">
-                    <label class="text-highlight" for="message">Message</label>
-                    <textarea id="message" name="message" cols="30" rows="10" required></textarea>
-                    @error('message')
-                    <p class="error">{{$message}}</p>
-                    @enderror
-                </div>
-                <div class="flex control-group">
-                    <button 
-                        type="submit"
-                        class="button button-blue"
-                    >
-                        Send
-                    </button>
-                </div>
-            </form>
+        <form 
+            action="contact-me" 
+            method="POST"
+            class="contact-form contact-one"
+            {{-- class="contact-grid-one contact-form" --}}
+        >    
+            @csrf
+            <div class="flex control-group">
+                <label class="text-highlight" for="name">Name</label>
+                <input id="name" name="name" type="text" required>
+                @error('name')
+                <p class="error">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="flex control-group">
+                <label class="text-highlight" for="email">Email</label>
+                <input id="email" name="email" type="text" required>
+                @error('email')
+                <p class="error">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="flex control-group">
+                <label class="text-highlight" for="message">Message</label>
+                <textarea id="message" name="message" cols="30" rows="10" required></textarea>
+                @error('message')
+                <p class="error">{{$message}}</p>
+                @enderror
+            </div>
+            <div class="flex control-group">
+                <button 
+                    type="submit"
+                    class="button button-blue"
+                >
+                    Send
+                </button>
+            </div>
+        </form>
         {{-- </div> --}}
     </div>
 </article>
